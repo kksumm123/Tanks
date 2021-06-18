576de2aa8fc4fc09951ae53f5851362a7d32fc3f
kksumm123 <kksumm123@gmail.com>
2021-06-19 02:29:57 +0900

    흠... Y값이 왜 움직이지

---
+++ Assets/cammove.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cammove : MonoBehaviour
+{
+    [SerializeField] Transform tankTr;
+    [SerializeField] float camSpeed = 1f;
+    Vector3 offset;
+    private void Start()
+    {
+        offset.x = transform.position.x - tankTr.position.x;
+        offset.y = transform.position.y - tankTr.position.y;
+        offset.z = transform.position.z - tankTr.position.z;
+        Debug.Log("::" + offset);
+    }
+
+    void Update()
+    {
+        //var pos = tankTr.position;
+
+        //transform.position = (tankTr.position + offset);
+
+        if (tankTr.position.y == 0)
+        {
+            var distance = (tankTr.position + offset) - transform.position;
+
+            distance.x *= Time.deltaTime * camSpeed;
+            distance.y = 0f;
+            distance.z *= Time.deltaTime * camSpeed;
+
+            transform.Translate(distance);
+        }
+    }
+}
