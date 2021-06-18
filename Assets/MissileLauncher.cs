74ca2c029ca65abbc8a47fa8bccb7b2a3935bb60
kksumm123 <kksumm123@gmail.com>
2021-06-18 21:17:51 +0900

    동숲에서처럼 스크린포인트레이 사용 ScreenPointToRay
    미사일을 레이쏴서 발사하기

--- Assets/MissileLauncher.cs
+++ Assets/MissileLauncher.cs
@@ -8,15 +8,29 @@ public class MissileLauncher : MonoBehaviourPun
     [SerializeField] GameObject missile;
     [SerializeField] Transform startPosition;
     [SerializeField] Vector3 offset;
+    [SerializeField] LayerMask layer;
+    [SerializeField] Transform canonBarrel;
     void Update()
     {
         if (photonView.IsMine == false)
             return;
 
-        if (Input.GetKeyDown(KeyCode.Return))
+        //if (Input.GetKeyDown(KeyCode.Return))
+        //{
+        //    Debug.Assert(missile != null, "미사일 없음");
+        //    PhotonNetwork.Instantiate(missile.name, startPosition.position, transform.rotation, 0);
+        //}
+        if (Input.GetMouseButton(0))
         {
-            Debug.Assert(missile != null, "미사일 없음");
-            PhotonNetwork.Instantiate(missile.name, startPosition.position, transform.rotation, 0);
+            var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if(Physics.Raycast(ray, out RaycastHit hitData,1000, layer))
+            {
+                var desPos = hitData.point;
+                var dir = desPos - transform.position;
+                canonBarrel.forward = dir;
+                //dir 방향으로 포신 돌리기
+                Instantiate(missile, startPosition.position, canonBarrel.rotation);
+            }
         }
     }
 }
